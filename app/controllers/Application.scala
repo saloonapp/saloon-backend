7b52729e40f3b74666cdf11c59d1b437285b276b
Loïc Knuchel <loicknuchel@gmail.com>
2015-05-24 19:58:38 +0200

    test date conversion

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,7 @@ import play.api.mvc._
 import play.api.libs.json.Json
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.DateTimeZone
 
 object Application extends Controller {
 
@@ -24,11 +25,15 @@ object Application extends Controller {
     val pattern = "yyyy-MM-dd HH:mm:ss.SSS"
     val f1 = DateTimeFormat.forPattern(pattern)
     val f2 = DateTimeFormat.forPattern(pattern).withLocale(java.util.Locale.FRANCE)
+    val f3 = DateTimeFormat.forPattern(pattern).withZone(DateTimeZone.forID("Europe/Paris"))
     val d1 = f1.parseDateTime(dateStr)
     val d2 = f2.parseDateTime(dateStr)
+    val d3 = f3.parseDateTime(dateStr)
     Ok(Json.obj(
+      "zone" -> DateTimeZone.getDefault().toString,
       "d1" -> Json.obj("str" -> d1.toString(), "iso" -> ISODateTimeFormat.dateTime().print(d1), "json" -> d1),
-      "d2" -> Json.obj("str" -> d2.toString(), "iso" -> ISODateTimeFormat.dateTime().print(d2), "json" -> d2)))
+      "d2" -> Json.obj("str" -> d2.toString(), "iso" -> ISODateTimeFormat.dateTime().print(d2), "json" -> d2),
+      "d3" -> Json.obj("str" -> d3.toString(), "iso" -> ISODateTimeFormat.dateTime().print(d3), "json" -> d3)))
   }
   /*def migrate(eventId: String) = Action.async {
     for {
