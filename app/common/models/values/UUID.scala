f296af8f432e1b99b9a37e791a7e6e7873948d74
Loïc Knuchel <loicknuchel@gmail.com>
2015-09-04 15:43:43 +0200

    improve uuid parsing

--- app/common/models/values/UUID.scala
+++ app/common/models/values/UUID.scala
@@ -8,10 +8,6 @@ trait UUID extends Any with tString {
   def toGenericId: GenericId = GenericId(this.unwrap)
 }
 object UUID {
-  // private val identifier = "([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})".r
   def generate(): String = java.util.UUID.randomUUID().toString()
-  def toUUID(str: String): Option[String] = Try(java.util.UUID.fromString(str)).toOption.map(_.toString) /*str match {
-    case identifier(id) => Some(id)
-    case _ => None
-  }*/
+  def toUUID(str: String): Option[String] = Try(java.util.UUID.fromString(str)).toOption.map(_.toString)
 }
