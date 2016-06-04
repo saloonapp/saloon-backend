4db296aa573e6cb5290f9bc2b1a4a234cba72d36
Loïc Knuchel <loicknuchel@gmail.com>
2016-06-04 22:10:23 +0200

    small refacto

--- app/backend/views/Events/Sessions/ScheduleHelper.scala
+++ app/backend/views/Events/Sessions/ScheduleHelper.scala
@@ -4,23 +4,21 @@ import common.models.event.Session
 import org.joda.time.DateTime
 
 object ScheduleHelper {
-  def hasTimeConflict(s1: Session, s2: Session): Boolean = {
+  def hasConflict(s1: Session, s2: Session): Boolean = {
     // s1.start = s2.start || s1.end = s2.end || (s1.start < s2.start < s1.end) || (s1.start < s2.end < s1.end) || (s2.start < s1.start && s1.end < s2.end)
-    s1.uuid != s2.uuid && (
-      s1.info.start.get.isEqual(s2.info.start.get) ||
-      s1.info.end.get.isEqual(s2.info.end.get) ||
-      (s1.info.start.get.isBefore(s2.info.start.get) && s2.info.start.get.isBefore(s1.info.end.get)) ||
-      (s1.info.start.get.isBefore(s2.info.end.get) && s2.info.end.get.isBefore(s1.info.end.get)) ||
-      (s2.info.start.get.isBefore(s1.info.start.get) && s1.info.end.get.isBefore(s2.info.end.get))
-    )
+    s1.info.start.get.isEqual(s2.info.start.get) ||
+    s1.info.end.get.isEqual(s2.info.end.get) ||
+    (s1.info.start.get.isBefore(s2.info.start.get) && s2.info.start.get.isBefore(s1.info.end.get)) ||
+    (s1.info.start.get.isBefore(s2.info.end.get) && s2.info.end.get.isBefore(s1.info.end.get)) ||
+    (s2.info.start.get.isBefore(s1.info.start.get) && s1.info.end.get.isBefore(s2.info.end.get))
   }
   def getConflicts(session: Session, allSessions: List[Session]): List[Session] = {
-    allSessions.filter(s => hasTimeConflict(session, s))
+    allSessions.filter(s => hasConflict(session, s))
   }
   def absolutePosition(session: Session, allSessions: List[Session], dayStart: DateTime, pxPerMinute: Int): String = {
     val startOffset = session.info.start.get.getMillis - dayStart.getMillis
     val duration = session.info.end.get.getMillis - session.info.start.get.getMillis
-    val conflicts = allSessions.filter(s => hasTimeConflict(session, s) || session.uuid == s.uuid)
+    val conflicts = getConflicts(session, allSessions)
     val width = 100/conflicts.length
     val left = conflicts.indexOf(session) * width
     s"top: ${toPx(startOffset, pxPerMinute)}px; " +
