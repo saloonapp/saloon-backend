70ba366f4913c2dd2e6b866f8da61a583cd10104
Loïc Knuchel <loicknuchel@gmail.com>
2016-07-06 14:02:13 +0200

    add Test Scheduler

--- app/common/services/Scheduler.scala
+++ app/common/services/Scheduler.scala
@@ -1,7 +1,9 @@
 package common.services
 
 import common.Utils
+import common.Defaults
 import java.util.concurrent.TimeUnit
+import common.models.values.typed.{TextHTML, Email}
 import common.repositories.conference.ConferenceRepository
 import conferences.models.Conference
 import conferences.services.TwittFactory
@@ -9,7 +11,7 @@ import org.joda.time.{DateTimeConstants, DateTime}
 import play.api.libs.json.Json
 import play.libs.Akka
 import scala.concurrent.Future
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.{FiniteDuration, Duration}
 import play.api.libs.concurrent.Execution.Implicits._
 
 object Scheduler {
@@ -17,16 +19,32 @@ object Scheduler {
     if(Utils.isProd()){
       NewsletterScheduler.init()
       TwittScheduler.init()
+      TestScheduler.init()
     }
+  }
+}
+
+object TestScheduler {
+  def init(): Unit = {
+    val next1 = SchedulerHelper.everyDayAt(23)(test1)
+    val next2 = SchedulerHelper.everyDayAt(5)(test2)
+    play.Logger.info("TestScheduler: next1 "+next1)
+    play.Logger.info("TestScheduler: next2 "+next2)
+  }
+  def test1(): Unit = {
+    val content = TextHTML("Hello from TestScheduler 1 (run at 23h00 every day)")
+    EmailSrv.sendEmail(EmailData("TestScheduler 1", Defaults.adminEmail, Email("loicknuchel@gmail.com"), "TEST 1", content, content.toPlainText))
+  }
+  def test2(): Unit = {
+    val content = TextHTML("Hello from TestScheduler 2 (run at 05h00 every day)")
+    EmailSrv.sendEmail(EmailData("TestScheduler 2", Defaults.adminEmail, Email("loicknuchel@gmail.com"), "TEST 2", content, content.toPlainText))
   }
 }
 
 object NewsletterScheduler {
   def init(): Unit = {
-    val now = new DateTime()
-    val next = SchedulerHelper.nextWeekDateTime(now, DateTimeConstants.MONDAY, 10)
+    val next = SchedulerHelper.everyWeekAt(DateTimeConstants.MONDAY, 10)(sendNewsletter)
     play.Logger.info("NewsletterScheduler: next newsletter on "+next)
-    Akka.system.scheduler.schedule(Duration(next.getMillis - now.getMillis, TimeUnit.MILLISECONDS), Duration(7, TimeUnit.DAYS))(sendNewsletter)
   }
   def sendNewsletter(): Unit = {
     play.Logger.info("NewsletterScheduler.sendNewsletter()")
@@ -64,10 +82,8 @@ object NewsletterScheduler {
 
 object TwittScheduler {
   def init(): Unit = {
-    val now = new DateTime()
-    val next = if(SchedulerHelper.isBeforeTime(now, 8)) now.withTime(8, 0, 0, 0) else now.plusDays(1).withTime(8, 0, 0, 0)
+    val next = SchedulerHelper.everyDayAt(10)(sendTwitts)
     play.Logger.info("TwittScheduler: next twitts on "+next)
-    Akka.system.scheduler.schedule(Duration(next.getMillis - now.getMillis, TimeUnit.MILLISECONDS), Duration(1, TimeUnit.DAYS))(sendTwitts)
   }
   def sendTwitts(): Unit = {
     play.Logger.info("TwittScheduler.sendTwitts()")
@@ -105,11 +121,26 @@ object TwittScheduler {
 }
 
 object SchedulerHelper {
-  def nextWeekDateTime(date: DateTime, weekDay: Int, hour: Int, minutes: Int = 0, seconds: Int = 0): DateTime = {
-    if(date.getDayOfWeek == weekDay && SchedulerHelper.isBeforeTime(date, hour, minutes, seconds)) date.withTime(hour, minutes, seconds, 0)
-    else SchedulerHelper.nextDayOfWeek(date, weekDay).withTime(hour, minutes, seconds, 0)
+  def everyWeekAt(weekDay: Int, hour: Int, minutes: Int = 0, seconds: Int = 0)(f: => Unit): DateTime =
+    everyAt(nextWeek(new DateTime(), weekDay, hour, minutes, seconds), Duration(7, TimeUnit.DAYS))(f)
+
+  def everyDayAt(hour: Int, minutes: Int = 0, seconds: Int = 0)(f: => Unit): DateTime =
+    everyAt(nextDay(new DateTime(), hour, minutes, seconds), Duration(1, TimeUnit.DAYS))(f)
+
+  private def everyAt(next: DateTime, interval: FiniteDuration)(f: => Unit): DateTime = {
+    val now = new DateTime()
+    Akka.system.scheduler.schedule(FiniteDuration(next.getMillis - now.getMillis, TimeUnit.MILLISECONDS), interval)(f)
+    next
   }
-  def nextDayOfWeek(date: DateTime, weekDay: Int): DateTime = date.plusDays((7 + weekDay - date.getDayOfWeek - 1) % 7 + 1)
+
+  def nextWeek(date: DateTime, weekDay: Int, hour: Int, minutes: Int = 0, seconds: Int = 0): DateTime = {
+    def nextDayOfWeek(date: DateTime, weekDay: Int): DateTime = date.plusDays((7 + weekDay - date.getDayOfWeek - 1) % 7 + 1)
+    if(date.getDayOfWeek == weekDay && SchedulerHelper.isBeforeTime(date, hour, minutes, seconds)) date.withTime(hour, minutes, seconds, 0)
+    else nextDayOfWeek(date, weekDay).withTime(hour, minutes, seconds, 0)
+  }
+  def nextDay(date: DateTime, hour: Int, minutes: Int = 0, seconds: Int = 0): DateTime = {
+    if(SchedulerHelper.isBeforeTime(date, hour, minutes, seconds)) date.withTime(hour, minutes, seconds, 0) else date.plusDays(1).withTime(hour, minutes, seconds, 0)
+  }
   def isBeforeTime(date: DateTime, hours: Int, minutes: Int = 0, seconds: Int = 0): Boolean = date.isBefore(date.withTime(hours, minutes, seconds, 0))
   def displayDuration(millis: Long): String = {
     val secs = millis/1000
