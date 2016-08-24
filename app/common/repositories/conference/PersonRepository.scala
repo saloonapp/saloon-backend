de822aca1335ff74677a337cf04a3ab935a81aa0
Loïc Knuchel <loicknuchel@gmail.com>
2016-08-25 01:44:37 +0200

    fix

--- app/common/repositories/conference/PersonRepository.scala
+++ app/common/repositories/conference/PersonRepository.scala
@@ -4,13 +4,13 @@ import common.repositories.CollectionReferences
 import common.repositories.utils.MongoDbCrudUtils
 import conferences.models.{PersonId, Person}
 import org.joda.time.DateTime
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.Play.current
 import play.api.libs.json.Json
 import play.modules.reactivemongo.ReactiveMongoPlugin
 import play.modules.reactivemongo.json.collection.JSONCollection
 import reactivemongo.api.commands.WriteResult
-
-import scala.concurrent.Future
 
 object PersonRepository {
   private val db = ReactiveMongoPlugin.db
@@ -18,7 +18,7 @@ object PersonRepository {
 
   // TODO : must change when allow to edit (like conferences & presentations)
   def find(): Future[List[Person]] = MongoDbCrudUtils.find[Person](collection, Json.obj(), Json.obj("name" -> 1))
-  def findByIds(ids: List[PersonId]): Future[List[Person]] = MongoDbCrudUtils.find[Person](collection, Json.obj("$or" -> ids.distinct.map(id => Json.obj("id" -> Json.obj("$eq" -> id)))), Json.obj("name" -> 1))
+  def findByIds(ids: List[PersonId]): Future[List[Person]] = ids.headOption.map(_ => MongoDbCrudUtils.find[Person](collection, Json.obj("$or" -> ids.distinct.map(id => Json.obj("id" -> Json.obj("$eq" -> id)))), Json.obj("name" -> 1))).getOrElse(Future(List()))
 
   def insert(elt: Person): Future[WriteResult] = MongoDbCrudUtils.insert(collection, elt.copy(created = new DateTime()))
 }
