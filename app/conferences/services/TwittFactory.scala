dd941e94cc29b7478f2a7516bcea7d80435012a8
Loïc Knuchel <l.knuchel@criteo.com>
2018-01-09 09:39:05 +0100

    twitts in english for conference list

--- app/conferences/services/TwittFactory.scala
+++ app/conferences/services/TwittFactory.scala
@@ -7,48 +7,48 @@ import org.joda.time.{Days, LocalDate}
 
 object TwittFactory {
   def newsletterSent(url: String): String = {
-    s"La newsletter des conférences tech françaises est là : $url via @getSalooN #conf #tech #dév"
+    s"Tech conference newsletter is there: $url via @getSalooN #conf #tech #cfp"
   }
   def newConference(c: Conference): String = {
     val url = Config.Application.baseUrl + conferences.controllers.routes.Conferences.detail(c.id)
-    val city = c.location.flatMap(_.locality).map(" à "+_).getOrElse("")
-    val cfp = c.cfp.map(", CFP ouvert jusqu'au "+_.end.toString(Config.Application.dateFormatter)).getOrElse("")
+    val city = c.location.flatMap(_.locality).map(" in "+_).getOrElse("")
+    val cfp = c.cfp.map(", CFP open until "+_.end.toString(Config.Application.dateFormatter)).getOrElse("")
     val account = c.twitterAccount.map(" cc @"+_).getOrElse("")
     val hashtag = c.twitterHashtag.map(" #"+_).getOrElse("")
-    val fullTwitt = s"${c.name} aura lieu le ${c.start.toString(Config.Application.dateFormatter)}$city$cfp $url$hashtag$account"
-    val smallTwitt = s"${c.name} aura lieu le ${c.start.toString(Config.Application.dateFormatter)}$city $url$hashtag$account"
+    val fullTwitt = s"${c.name} will take place on ${c.start.toString(Config.Application.dateFormatter)}$city$cfp $url$hashtag$account"
+    val smallTwitt = s"${c.name} will take place on ${c.start.toString(Config.Application.dateFormatter)}$city $url$hashtag$account"
     if(fullTwitt.length + 20 - url.length < 140) fullTwitt else smallTwitt
   }
   def publishVideos(c: Conference): String = {
     val url = Config.Application.baseUrl + conferences.controllers.routes.Conferences.detail(c.id)
     val name = c.twitterAccount.map("@"+_).getOrElse(c.name)
     val hashtag = c.twitterHashtag.map(" #"+_).getOrElse("")
-    s"Publication des vidéos de $name $url$hashtag"
+    s"Videos for $name just have been published: $url$hashtag"
   }
   def openCfp(c: Conference): String = {
     val url = Config.Application.baseUrl + conferences.controllers.routes.Conferences.detail(c.id)
     val name = c.twitterAccount.map("@"+_).getOrElse(c.name)
     val hashtag = c.twitterHashtag.map(" #"+_).getOrElse("")
-    s"Ouverture du CFP de $name $url$hashtag"
+    s"Opening of the $name CFP: $url$hashtag"
   }
   def closingCFP(c: Conference, now: LocalDate): String = {
     val url = Config.Application.baseUrl + conferences.controllers.routes.Conferences.detail(c.id)
     val name = c.twitterAccount.map("@"+_).getOrElse(c.name)
     val hashtag = c.twitterHashtag.map(" #"+_).getOrElse("")
-    s"Fermeture du CFP de $name ${dayDuration(now, c.cfp.get.end)} $url$hashtag"
+    s"Closing of the $name CFP ${dayDuration(now, c.cfp.get.end)} $url$hashtag"
   }
   def startingConference(c: Conference, now: LocalDate): String = {
     val url = Config.Application.baseUrl + conferences.controllers.routes.Conferences.detail(c.id)
     val name = c.twitterAccount.map("@"+_).getOrElse(c.name)
     val hashtag = c.twitterHashtag.map(" #"+_).getOrElse("")
-    s"Début de $name ${dayDuration(now, c.start)} $url$hashtag"
+    s"Beginning of $name ${dayDuration(now, c.start)} $url$hashtag"
   }
 
   def genericRemindToAddSlides(c: Conference): String = { // à 8h30, 12h, 16h et 19h pendant la conf
     val url = Config.Application.baseUrl + conferences.controllers.routes.Conferences.detail(c.id)
     val account = c.twitterAccount.map("@"+_).getOrElse(c.name)
     val hashtag = c.twitterHashtag.map(" #"+_).getOrElse("")
-    s"Speaker à $account ? Publie tes slides sur $url$hashtag"
+    s"Speaker at $account ? Publish your slides here $url ;)$hashtag"
   }
   def replySuggestToAddSlides(c: Conference, tweet: SimpleTweet): String = {
     val url = Config.Application.baseUrl + conferences.controllers.routes.Conferences.detail(c.id)
@@ -63,16 +63,16 @@ object TwittFactory {
     val url = Config.Application.baseUrl + conferences.controllers.routes.Conferences.detail(c.id)
     val account = c.twitterAccount.map("@"+_).getOrElse(c.name)
     val hashtag = c.twitterHashtag.map(" #"+_).getOrElse("")
-    s"Des présentationss (slides et vidéos) ont été publiées pour $account $url$hashtag"
+    s"Presentations (slides et videos) have been published for $account $url$hashtag"
   }*/
 
   private def dayDuration(start: LocalDate, end: LocalDate): String = {
     Days.daysBetween(start, end).getDays() match {
-      case 0 => "aujourd'hui"
-      case 1 => "demain"
-      case 7 => "dans une semaine"
-      case 14 => "dans deux semaines"
-      case n => s"dans $n jours"
+      case 0 => "today"
+      case 1 => "tomorrow"
+      case 7 => "in one week"
+      case 14 => "in two weeks"
+      case n => s"in $n days"
     }
   }
 }
